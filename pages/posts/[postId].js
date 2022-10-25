d835d8bb0d28d1326ec2c421c66e84874ba792a6
Moonshadow <last_enemy@naver.com>
2022-10-25 14:53:21 +0900

    Static Site Generation

---
+++ pages/posts/[postId].js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const PostId = ({ post }) => (
+  <>
+    <h2>
+      {post.id} {post.title}
+    </h2>
+    <p>{post.body}</p>
+  </>
+);
+
+export default PostId;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: '1' },
+      },
+      {
+        params: { postId: '2' },
+      },
+      {
+        params: { postId: '3' },
+      },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const data = await (
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+  ).json();
+  return {
+    props: { post: data },
+  };
+}
